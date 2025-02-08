fe62ba306d81129b25bb97a7ba73cb4798ddf8ad
lemanrza <lemaner@code.edu.az>
2025-02-08 20:12:28 +0400

    deploy pages

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: "index.html",
+                favorites: "favorites.html",
+                addMv: "addMv.html",
+                edit: "edit.html",
+                detail: "detail.html"
+            }
+        }
+    }
+})
